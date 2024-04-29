110bab86f8871b829eccc6c41cc2efdcdc68fe35
apeterlein <11824265+apeterlein@users.noreply.github.com>
2024-04-29 12:24:14 -0600

    v2

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 window.addEventListener("DOMContentLoaded", () => {
 	"use strict";
 
+	function startLoading() {
+		document.getElementById("loading").classList.remove("hidden");
+		document.getElementById("grid").classList.add("hidden");
+	}
+	function endLoading() {
+		requestAnimationFrame(() => setTimeout(() => {
+			document.getElementById("loading").classList.add("hidden");
+			document.getElementById("grid").classList.remove("hidden");
+		}, 0));
+	}
+
+	const colors = (() => {
+		let result = [[255, 255, 255]];
+		for (let i = 0; ; i++) {
+			const elm = document.getElementById("color-" + i);
+			if (!elm) break;
+			const color = window.getComputedStyle(elm).backgroundColor;
+			result.push([...color.matchAll(/\d+/g)].map((x) => parseInt(x[0]) ));
+		}
+		return result;
+	})();
+
+	function closestColor(c1) {
+		let distance;
+		let final;
+		for (const i in colors) {
+			const r = c1[0] - colors[i][0];
+			const g = c1[1] - colors[i][1];
+			const b = c1[2] - colors[i][2];
+			const dist = r*r + g*g + b*b;
+			if (!distance || dist < distance) {
+				distance = dist;
+				final = colors[i];
+			}
+		}
+		return "rgb(" + final[0] + "," + final[1] + "," + final[2] + ")";
+	}
+
 	function invertColor(color) {
 		let output = 0;
 		[...color.matchAll(/\d+/g)].forEach((match) => {
@@ -10,6 +48,10 @@ window.addEventListener("DOMContentLoaded", () => {
 			return "black"
 		}
 		return "white"
+	}
+
+	function getSquare(i, j) {
+		return document.querySelector("#grid > :nth-child(" + (i+2) + "n) > .square:nth-child(" + (j+2) + "n)");
 	}
 
 	function keyInput(e) {
@@ -24,9 +66,17 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 
+	let shift = false;
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "Shift") shift = true;
+	});
+	document.addEventListener("keyup", (e) => {
+		if (e.key === "Shift") shift = false;
+	});
+
 	function refreshGrid() {
-		const rows = document.getElementById("rows").innerHTML;
-		const cols = document.getElementById("cols").innerHTML;
+		const rowCnt = document.getElementById("rows").innerHTML;
+		const colCnt = document.getElementById("cols").innerHTML;
 		const grid = document.createElement("div");
 
 		const row = document.createElement("div");
@@ -34,7 +84,7 @@ window.addEventListener("DOMContentLoaded", () => {
 		const cell = document.createElement("div");
 		cell.classList.add("label");
 		row.append(cell);
-		for (let j = 0; j < cols; j++) {
+		for (let j = 0; j < colCnt; j++) {
 			const cell = document.createElement("div");
 			cell.classList.add("label");
 			cell.innerHTML = j + 1;
@@ -42,16 +92,16 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 		grid.append(row);
 
-		for (let i = 0; i < rows; i++) {
+		for (let i = 0; i < rowCnt; i++) {
 			const row = document.createElement("div");
 			row.classList.add("row-reverse");
 			const label = document.createElement("div");
 			label.classList.add("label")
 			label.innerHTML = i + 1;
 			row.append(label);
-			for (let j = 0; j < cols; j++) {
+			for (let j = 0; j < colCnt; j++) {
 				let cell;
-				const prv = document.querySelector("#grid > :nth-child(" + (i+2) + "n) > .square:nth-child(" + (j+2) + "n)");
+				const prv = getSquare(i, j);
 				if (prv) {
 					cell = prv.cloneNode(true);
 				}
@@ -89,25 +139,51 @@ window.addEventListener("DOMContentLoaded", () => {
 		});
 	}
 
+	document.getElementById("add-zoom").addEventListener("click", () => {
+		const zoom = document.getElementById("zoom");
+		let ratio = parseFloat(zoom.innerHTML) + 10;
+		zoom.innerHTML = ratio + "%";
+		document.documentElement.style.setProperty("--square-size", ratio / 100 * 3.5 + "em");
+	});
+	document.getElementById("sub-zoom").addEventListener("click", () => {
+		const zoom = document.getElementById("zoom");
+		let ratio = parseFloat(zoom.innerHTML) - 10;
+		ratio = ratio < 10 ? 10 : ratio;
+		zoom.innerHTML = ratio + "%";
+		document.documentElement.style.setProperty("--square-size", ratio / 100 * 3.5 + "em");
+	});
+
 	document.getElementById("add-col").addEventListener("click", () => {
-		document.getElementById("cols").innerHTML++;
+		const cols = document.getElementById("cols");
+		cols.innerHTML = parseInt(cols.innerHTML) + (shift ? 10 : 1);
+		if (cols.innerHTML > 1000) {
+			cols.innerHTML = 1000;
+		}
 		refreshGrid();
 	});
 	document.getElementById("sub-col").addEventListener("click", () => {
-		if (document.getElementById("cols").innerHTML > 1) {
-			document.getElementById("cols").innerHTML--;
-			refreshGrid();
+		const cols = document.getElementById("cols");
+		cols.innerHTML = parseInt(cols.innerHTML) - (shift ? 10 : 1);
+		if (cols.innerHTML < 1) {
+			cols.innerHTML = 1;
 		}
+		refreshGrid();
 	});
 	document.getElementById("add-row").addEventListener("click", () => {
-		document.getElementById("rows").innerHTML++;
+		const rows = document.getElementById("rows");
+		rows.innerHTML = parseInt(rows.innerHTML) + (shift ? 10 : 1);
+		if (rows.innerHTML > 1000) {
+			rows.innerHTML = 1000;
+		}
 		refreshGrid();
 	});
 	document.getElementById("sub-row").addEventListener("click", () => {
-		if (document.getElementById("rows").innerHTML > 1) {
-			document.getElementById("rows").innerHTML--;
-			refreshGrid();
+		const rows = document.getElementById("rows");
+		rows.innerHTML = parseInt(rows.innerHTML) - (shift ? 10 : 1);
+		if (rows.innerHTML < 1) {
+			rows.innerHTML = 1;
 		}
+		refreshGrid();
 	});
 
 	Array.prototype.forEach.call(document.querySelectorAll(".selectable"), (elm) => {
@@ -154,15 +230,17 @@ window.addEventListener("DOMContentLoaded", () => {
 		const file = document.createElement("input");
 		file.type = "file";
 		file.accept = ".knit";
-		file.dispatchEvent(new MouseEvent("click"));
-		file.addEventListener("change", async () => {
-			const input = Buffer.from(await file.files[0].arrayBuffer());
-			const output = Buffer.alloc(1e6);
-			document.getElementById("grid").innerHTML = new TextDecoder().decode(output.subarray(0, LZ4.decodeBlock(input, output)));
-			const rows = document.getElementById("grid").childNodes;
-			document.getElementById("rows").innerHTML = rows.length - 1;
-			document.getElementById("cols").innerHTML = rows[0].childElementCount - 1;
-			refreshGrid();
+		setTimeout(() => file.dispatchEvent(new MouseEvent("click")), 0);
+		file.addEventListener("change", () => {
+			file.files[0].arrayBuffer().then((buffer) => {
+				const input = Buffer.from(buffer);
+				const output = Buffer.alloc(1e9);
+				document.getElementById("grid").innerHTML = new TextDecoder().decode(output.subarray(0, LZ4.decodeBlock(input, output)));
+				const rows = document.getElementById("grid").childNodes;
+				document.getElementById("rows").innerHTML = rows.length - 1;
+				document.getElementById("cols").innerHTML = rows[0].childElementCount - 1;
+				refreshGrid();
+			});
 		});
 	});
 	document.getElementById("export").addEventListener("click", () => {
@@ -204,6 +282,34 @@ window.addEventListener("DOMContentLoaded", () => {
 			});
 		});
 	});
+	document.getElementById("import").addEventListener("click", () => {
+		const file = document.createElement("input");
+		file.type = "file";
+		file.accept = "image/*";
+		setTimeout(() => file.dispatchEvent(new MouseEvent("click")), 0);
+		file.addEventListener("change", () => {
+			createImageBitmap(file.files[0]).then((img) => {
+				const width = parseInt(document.getElementById("cols").innerHTML);
+				const height = Math.round(img.height / img.width * width);
+				document.getElementById("rows").innerHTML = height;
+				refreshGrid();
+
+				const canvas = document.createElement("canvas");
+				const ctx = canvas.getContext("2d");
+				ctx.drawImage(img, 0, 0, width, height);
+				const data = ctx.getImageData(0, 0, width, height, { colorSpace: "srgb" }).data;
+				
+				for (let i = 0; i < height; i++) {
+					for (let j = 0; j < width; j++) {
+						const square = getSquare(i, j);
+						const pixel = ((height - i) * width + (width - j)) * 4;
+						square.style.backgroundColor = closestColor(data.slice(pixel, pixel + 3));
+					}
+				}
+			});
+		});
+	});
 
 	refreshGrid();
+	endLoading();
 });
